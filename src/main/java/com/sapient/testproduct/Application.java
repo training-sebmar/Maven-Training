aed59c171c0e4d1c221efb3a66b0a8de2f9293cf
Sebin Maret <seb17jan@gmail.com>
2021-06-22 16:13:10 +0530

    first commit

---
+++ src/main/java/com/sapient/testproduct/Application.java
@@ -0,0 +1,13 @@
+package com.sapient.testproduct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+	static final Logger logger=LoggerFactory.getLogger(Application.class);
+	public static void main(String[] args) {
+		logger.debug("Hello world");
+
+	}
+
+}
